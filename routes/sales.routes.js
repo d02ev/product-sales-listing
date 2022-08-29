86493968c2e0ee1e54bd173392e59df11ca44a6e
d02ev <vikramadityapratapsingh98@gmail.com>
2022-08-29 18:34:37 +0530

    Update sales.routes.js

--- routes/sales.routes.js
+++ routes/sales.routes.js
@@ -2,6 +2,6 @@ const Route = require('express').Router();
 const SalesController = require('../controllers/sales.controllers');
 
 // add a sales record in the db
-Route.post('/', SalesController.APICreateSalesRecord);
+Route.post('/add', SalesController.APICreateSalesRecord);
 
 module.exports = Route;
