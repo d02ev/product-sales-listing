ca2942bd285364ca0cfa249c10e9a1b594d27617
d02ev <vikramadityapratapsingh98@gmail.com>
2022-08-29 20:08:06 +0530

    Update sales.routes.js

--- routes/sales.routes.js
+++ routes/sales.routes.js
@@ -3,5 +3,7 @@ const SalesController = require('../controllers/sales.controllers');
 
 // add a sales record in the db
 Route.post('/add', SalesController.APICreateSalesRecord);
+// top 5 selling products
+Route.get('/top', SalesController.APIGetTopSellingProducts);
 
 module.exports = Route;
