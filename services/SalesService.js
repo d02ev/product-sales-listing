5f93e779b45c97824442f46671ef9ffc643060ba
d02ev <vikramadityapratapsingh98@gmail.com>
2022-08-29 18:25:46 +0530

    Add SalesService.js

---
+++ services/SalesService.js
@@ -0,0 +1,30 @@
+const SalesModel = require('../models/SalesSchema');
+
+module.exports = class SalesService {
+    static async createSalesRecord(creationData) {
+        try {
+            const new_sales_record = {
+                name: creationData.name,
+                quantity: creationData.quantity,
+                amount: creationData.amount,
+                date: creationData.date
+            }
+
+            const response = await new SalesModel(creationData).save();
+            return response;
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    static async getSalesRecordByProductName(productName) {
+        try {
+            const record_by_prod_name = await SalesModel.findOne({ name: productName });
+            return record_by_prod_name;
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+}
