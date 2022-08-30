16e32d6ffabaf00abc7c183f9dfea5626afe17b0
d02ev <vikramadityapratapsingh98@gmail.com>
2022-08-30 21:23:15 +0530

    SalesService.js

--- services/SalesService.js
+++ services/SalesService.js
@@ -17,10 +17,10 @@ module.exports = class SalesService {
         }
     }
 
-    static async getAllSalesRecords() {
+    static async getTopSalesRecords() {
         try {
-            const all_records = await SalesModel.find();
-            return all_records;
+            const top_records = await SalesModel.find().sort({ quantity: -1 }).limit(5);
+            return top_records;
         }
         catch (err) {
             console.error(err);
@@ -31,6 +31,19 @@ module.exports = class SalesService {
         try {
             const record_by_prod_name = await SalesModel.findOne({ name: productName });
             return record_by_prod_name;
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    static async getSalesRecordsOfToday() {
+        try {
+            const records_by_date = await SalesModel.find({
+                createdAt: { $gt: new Date(Date.now() - 86400000) }
+            }).sort({ createdAt: -1 });
+            
+            return records_by_date;
         }
         catch (err) {
             console.error(err);
